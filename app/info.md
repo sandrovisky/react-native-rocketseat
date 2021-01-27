8e4416c263a7f0072e33422c09a5755bc03bd49c
Sandro Garcia <sndr95@hotmail.com>
2021-01-27 15:37:39 -0800

    termino do exemplo

---
+++ app/info.md
@@ -0,0 +1,8 @@
+não possuem valor semântico
+não possuem estilização própria
+todos componentes possuem por padrão 'display: flex'
+
+View: div, footer, header, main, aside, section
+Text: p, span, strong, h1, h2, h3
+
+
--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,54 @@
-import React from 'react'
-import {View, Text, StyleSheet, StatusBar} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import {SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity} from 'react-native'
+
+import api from './services/api'
 
 export default function App() {
+
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            console.log(response.data)
+            setProjects(response.data)
+        })
+    }, [])
+    
+    async function handleAddProject() {
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: 'chocolate'
+        })
+
+        setProjects([...projects, response.data])
+    }
+
     return (
     <>
         <StatusBar barStyle = 'light-content' backgroundColor = '#7159c1' />
-        <View style = {styles.container} >
-            <Text style = {styles.title}>Ola Mundão!</Text>
-        </View>
+
+        <SafeAreaView style = {styles.container}>
+            <FlatList 
+                data = {projects}
+                keyExtractor= {project => project.id}
+                renderItem= {({ item:project }) => (
+                    <Text style= {styles.project}>{project.title}</Text>
+                )}
+            /> 
+
+            <TouchableOpacity 
+                activeOpacity = {0.6} 
+                style = {styles.button} 
+                onPress= {handleAddProject}>
+                    <Text style = {styles.buttonText}>Adicionar projeto</Text>
+            </TouchableOpacity> 
+        </SafeAreaView>
+        
+        {/*<View style = {styles.container} >
+            {projets.map(project => ( 
+                <Text style= {styles.project} key={project.id}>{project.title}</Text>
+             ))}
+        </View>*/}
     </>
     )
 }
@@ -16,8 +57,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#7159c1',
-        justifyContent: 'center',
-        alignItems: 'center'
     },
 
     title: {
@@ -26,4 +65,23 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         
     },
+
+    project: {
+        color: '#FFF',
+        fontSize: 20,
+    },
+
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
 });
